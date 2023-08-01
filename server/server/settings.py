f0b7f69fd938f96bedb15664cb45ca630b7fae7a
Ben Lim <limzhengwei1002@gmail.com>
2023-08-02 03:35:50 +0800

    commit

--- server/server/settings.py
+++ server/server/settings.py
@@ -21,14 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ud3%tp$9zstk#b4t!*t=pk6d=ei(l1mb$=2qqtyfo8_q484ld('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [
-    '127.0.0.1'
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
